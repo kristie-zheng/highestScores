cfd73fe5c3e6faf9fcbcfdd50d316daa05fed6a6
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-22 21:34:24 -0700

    Implement try/catch to handle malformed JSON data

--- findHighestScore.js
+++ findHighestScore.js
@@ -6,21 +6,30 @@ let parseFile = inputFilePath => {
   let records = [];
   const input = fs.createReadStream(inputFilePath);
   const rl = readline.createInterface(input);
+  let lineNumber = 1;
+
   rl.on('line', (line) => {
     console.log('heres the line', line);
     if (line) {
-      var segments = line.split(/[0-9]\:/);
-      var score = Number(segments[0]);
-      var data = JSON.parse(segments[1]);
-      console.log('score', score, 'segments', data)
-      if (data.id) {
-        let record = {};
-        record.id = data.id;
-        record.score = score;
-        records.push(record);
+      let segments = line.split(/[0-9]\:/);
+      let score = Number(segments[0]);
+      try {
+        let data = JSON.parse(segments[1]);
+        if (data.id) {
+          let record = {};
+          record.id = data.id;
+          record.score = score;
+          records.push(record);
+          console.log('score', score, 'segments', data)
+        }
+      }
+      catch(exception) {
+        console.log("Problem parsing JSON at row", lineNumber, exception)
       }
     }
+    lineNumber++
   });
+  
   rl.on('close', () => {
     console.log('here is the records array', records);
   })
