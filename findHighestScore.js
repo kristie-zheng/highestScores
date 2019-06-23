7340342c20afbe7bd2a91a299596bbfd0c3fdc59
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-23 15:54:32 -0700

    Add comments for each function

--- findHighestScore.js
+++ findHighestScore.js
@@ -3,9 +3,9 @@ const readline = require('readline');
 const buckets = require('buckets-js');
 
 /*
-  function to read in text file and retrieve id and score
-  takes inputFilePath (string)
-  returns array of objects (records) containing id and score
+  function to read in text file and produce array of records
+  takes inputFilePath, a callback, and a number of records desired [pass to CB]
+  returns result of the callback: an array of the n highest scores
 */
 let parseFile = (inputFilePath, afterParsed, n) => {
   let records = [];
@@ -14,7 +14,7 @@ let parseFile = (inputFilePath, afterParsed, n) => {
   let lineNumber = 1;
 
   rl.on('line', (line) => {
-    console.log('heres the line', line);
+    // console.log('heres the line', line);
     if (line) {
       let segments = line.split(/[0-9]\:/);
       let score = Number(segments[0]);
@@ -25,7 +25,7 @@ let parseFile = (inputFilePath, afterParsed, n) => {
           record.id = data.id;
           record.score = score;
           records.push(record);
-          console.log('score', score, 'segments', data)
+          // console.log('score', score, 'segments', data)
         }
       }
       catch(exception) {
@@ -39,13 +39,13 @@ let parseFile = (inputFilePath, afterParsed, n) => {
     console.log('here is the records array', records);
     afterParsed(records, n);
   });
-  return records;
 }
 
 /*
-  function that retrieves n top scores from an array
+  function that retrieves n top scores from an array of records
   takes in an input array and a number of top records to retrieve
   returns array of the n top scores
+  used as callback to parseFile function
 */
 
 let comparator = (objectA, objectB) => {
@@ -70,19 +70,21 @@ let findHighestScores = (records, n) => {
     let removedVal = minHeap.removeRoot();
     nHighestScores.push(removedVal);
   }
-  console.log('here are the', n, 'highstScores', nHighestScores);
-  return nHighestScores;
+  console.log('here are the', n, 'highstScores', nHighestScores.reverse());
+  return nHighestScores.reverse();
 }
 
 /*
   "main function"
   function that finds n highest scores
   take in file path and n
-  output / log json file
+  invokes parseFile
+  outputs / log json file
  */
 
 let nHighestByFile = (filePath, n) => {
   let dataRecords = parseFile(filePath, findHighestScores, n);
+  console.log('datarecords', dataRecords);
 }
 
 console.log(nHighestByFile('./data.txt', 3))
