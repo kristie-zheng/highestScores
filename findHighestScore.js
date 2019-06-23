133eccde1a7016a0ccd2cd9a89cd76f59ad1f9fe
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-22 21:04:32 -0700

    Move record creation inside conditional so empty records arent made for lines without id field

--- findHighestScore.js
+++ findHighestScore.js
@@ -3,25 +3,26 @@ const readline = require('readline');
 
 //function to read in text file and retrieve id and score
 let parseFile = inputFilePath => {
+  let records = [];
   const input = fs.createReadStream(inputFilePath);
   const rl = readline.createInterface(input);
   rl.on('line', (line) => {
-    var record = {};
     console.log('heres the line', line);
     if (line) {
       var segments = line.split(/[0-9]\:/);
       var score = Number(segments[0]);
       var data = JSON.parse(segments[1]);
       console.log('score', score, 'segments', data)
-      if (data['id']) {
-        record.id = data['id'];
+      if (data.id) {
+        let record = {};
+        record.id = data.id;
         record.score = score;
+        records.push(record);
       }
-      console.log(record)
     }
   });
   rl.on('close', () => {
-    console.log('done');
+    console.log('here is the records array', records);
   })
 }
 
