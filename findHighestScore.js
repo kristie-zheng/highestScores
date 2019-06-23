1289b9b2b665ddf68a48bcba43e0ab47832771ed
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-23 14:57:15 -0700

    Create function that uses MinHeap to find n top scored records

--- findHighestScore.js
+++ findHighestScore.js
@@ -2,7 +2,11 @@ const fs = require('file-system');
 const readline = require('readline');
 const buckets = require('buckets-js');
 
-//function to read in text file and retrieve id and score
+/*
+  function to read in text file and retrieve id and score
+  takes inputFilePath (string)
+  returns array of objects (records) containing id and score
+*/
 let parseFile = inputFilePath => {
   let records = [];
   const input = fs.createReadStream(inputFilePath);
@@ -33,10 +37,15 @@ let parseFile = inputFilePath => {
 
   rl.on('close', () => {
     console.log('here is the records array', records);
-  })
+  });
+  return records;
 }
 
-parseFile('./data.txt');
+/*
+  function that retrieves n top scores from an array
+  takes in an input array and a number of top records to retrieve
+  returns array of the n top scores
+*/
 
 let comparator = (objectA, objectB) => {
   if (objectA.score === objectB.score) {
@@ -45,6 +54,21 @@ let comparator = (objectA, objectB) => {
   return objectA.score < objectB.score ? -1 : 1;
 }
 
-let findHighestScore = (n) => {
+let findHighestScores = (records, n) => {
+  var nHighestScores = [];
   let minHeap = new buckets.Heap(comparator);
+  records.forEach(record => {
+    if (record.score > minHeap.peek()) {
+      minHeap.add(record);
+    }
+    if (minHeap.size() > n) {
+      minHeap.removeRoot();
+    }
+  })
+  while (minHeap.size() > 0) {
+    let removedVal = minHeap.removeRoot();
+    nHighestScores.push(removedVal);
+  }
+  console.log('here are the', n, 'highstScores', nHighestScores);
+  return nHighestScores;
 }
