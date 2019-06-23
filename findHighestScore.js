ac2eb250c4a9569dfd0287007a148f0e1c30e611
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-22 20:48:01 -0700

    Assign score and data to variables

--- findHighestScore.js
+++ findHighestScore.js
@@ -7,9 +7,14 @@ let parseFile = inputFilePath => {
   const rl = readline.createInterface(input);
   rl.on('line', (line) => {
     console.log('heres the line', line);
-    var segments = line.split(/[0-9]\:/);
-    console.log(segments);
-
+    if (line) {
+      var segments = line.split(/[0-9]\:/);
+      console.log(segments);
+      var score = Number(segments[0]);
+      var data = segments[1];
+      console.log('score', score, 'segments', data)
+      console.log('parsed', JSON.parse(data))
+    }
   });
   rl.on('close', () => {
     console.log('done');
